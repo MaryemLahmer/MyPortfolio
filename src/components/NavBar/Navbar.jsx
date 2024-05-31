1fd1a57333eacddaf17a7b982fa69ab4a8e58b4b
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-05-31 19:06:36 +0100

    formatting

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -7,14 +7,16 @@ export const Navbar = () => {
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">My Portfolio</a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} 
-            src={
-                menuOpen ?
-                getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")}
-            alt="menu-button"
-            onClick={() => setMenuOpen(!menuOpen)}
-        
-            />
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
             <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
             onClick={() => setMenuOpen(false)}
             >
