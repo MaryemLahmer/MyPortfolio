a4b9f12ae07998f5ec555b2a96be189eabc7e3b1
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2025-02-08 23:32:07 +0100

    update project section

--- src/components/Projetcs/ProjectCard.jsx
+++ src/components/Projetcs/ProjectCard.jsx
@@ -25,9 +25,6 @@ export const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>
-          Demo
-        </a>
         <a href={source} className={styles.link}>
           Source
         </a>
